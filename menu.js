f4ad483f47e2118c1fbfc7fc4d5ab8692e02e7dc
msrusnac <135354079+msrusnac@users.noreply.github.com>
2023-08-16 13:08:56 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,21 @@
+let popupBg = document.querySelector('.menu--vertical'); // Фон попап окна
+let openPopupButtons = document.querySelectorAll('.hamburger'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close__icon-wrapper'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+  })
+});
+
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+  }
+});
