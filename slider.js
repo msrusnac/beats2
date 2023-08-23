241ac07ffaeb26487098ef30c0ca4754498f09d5
msrusnac <135354079+msrusnac@users.noreply.github.com>
2023-08-23 13:23:03 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,21 +1,37 @@
-const left = document.querySelector("#left");
-const right = document.querySelector("#right");
-const itemsList = document.querySelector("#items");
+// const left = document.querySelector("#left");
+// const right = document.querySelector("#right");
+// const itemsList = document.querySelector("#items");
 
-const loop = (direction, e) => {
+// const loop = (direction, e) => {
+//   e.preventDefault();
+
+//   if (direction === "right") {
+//     itemsList.appendChild(itemsList.firstElementChild);
+//   } else {
+//     itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
+//   }
+// };
+
+// right.addEventListener("click", (e) => {
+//   loop("right", e);
+// });
+
+// left.addEventListener("click", (e) => {
+//   loop("left", e);
+// });
+
+
+const slider = $('.products').bxSlider({
+  pager: false,
+  controls: false
+});
+
+$('.product-slider__arow--prev').click(e => {
   e.preventDefault();
+  slider.goToPrevSlide();
+})
 
-  if (direction === "right") {
-    itemsList.appendChild(itemsList.firstElementChild);
-  } else {
-    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
-  }
-};
-
-right.addEventListener("click", (e) => {
-  loop("right", e);
-});
-
-left.addEventListener("click", (e) => {
-  loop("left", e);
-});
+$('.product-slider__arow--next').click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})
